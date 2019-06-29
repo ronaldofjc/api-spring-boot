6c73f5dbcd7eed470e88a33ccd8603b49b4cf839
Ronaldo <rfeitosa@ciandt.com>
2019-06-29 13:13:45 -0300

    Initial Commit

---
+++ 00-api-rest-with-spring-boot/src/main/java/br/com/ronaldofjc/apirestwithspringboot/ApiRestWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.ronaldofjc.apirestwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestWithSpringBootApplication.class, args);
+	}
+
+}
